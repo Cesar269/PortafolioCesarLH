b7b6e5255fd5257149d56e990f777226f308732f
Cesar269 <cesarin-268@hotmail.com>
2021-09-19 12:53:44 -0500

    inicio

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import NavBar from '../src/components/NavBar';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <NavBar></NavBar>
   </React.StrictMode>,
   document.getElementById('root')
 );
